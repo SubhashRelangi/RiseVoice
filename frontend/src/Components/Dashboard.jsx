1eccf378ed795d5c075a28ae23757832e1679ea2
SubhashRelangi <subhashrelangi@gmail.com>
2025-08-28 20:55:51 +0530

    feat: Add Dashboard component
    This commit introduces the new Dashboard component and integrates it into the HomePage. It also includes the necessary styling for the component.

---
+++ frontend/src/Components/Dashboard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../StyleSheets/Dashboard.css'
+
+const Dashboard = () => {
+    return (
+        <div className='DashboardDiv'>
+            <div className='DashboardContent'>
+                <h1 className='Heading'>5000+</h1>
+                <p className='Para'>Problems</p>
+            </div>
+            <div className='DashboardContent'>
+                <h1 className='Heading'>2500+</h1>
+                <p className='Para'>Sloved</p>
+            </div>
+            <div className='DashboardContent'>
+                <h1 className='Heading'>600+</h1>
+                <p className='Para'>In-Progess</p>
+            </div>
+            <div className='DashboardContent'>
+                <h1 className='Heading'>1500+</h1>
+                <p className='Para'>Today Raised</p>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
+
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from '../Components/Navigation'
 import HeroSection from '../Components/HeroSection'
+import Dashboard from '../Components/Dashboard'
 
 const HomePage = () => {
   return (
     <div>
       <Navigation />
       <HeroSection />
+      <Dashboard />
     </div>
   )
 }
