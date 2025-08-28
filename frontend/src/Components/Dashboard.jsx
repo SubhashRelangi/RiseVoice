35f56cfddc6dc725854ee39ba70ad8d563342631
SubhashRelangi <subhashrelangi@gmail.com>
2025-08-28 21:00:57 +0530

    style: Adjust Dashboard component layout
    This commit refines the visual layout of the Dashboard component.
    
    - A DashboardContainer has been introduced to better structure the component's elements.
    
    - The width of the DashboardContent has been adjusted for a more balanced and visually appealing presentation.

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -4,21 +4,23 @@ import '../StyleSheets/Dashboard.css'
 const Dashboard = () => {
     return (
         <div className='DashboardDiv'>
-            <div className='DashboardContent'>
-                <h1 className='Heading'>5000+</h1>
-                <p className='Para'>Problems</p>
-            </div>
-            <div className='DashboardContent'>
-                <h1 className='Heading'>2500+</h1>
-                <p className='Para'>Sloved</p>
-            </div>
-            <div className='DashboardContent'>
-                <h1 className='Heading'>600+</h1>
-                <p className='Para'>In-Progess</p>
-            </div>
-            <div className='DashboardContent'>
-                <h1 className='Heading'>1500+</h1>
-                <p className='Para'>Today Raised</p>
+            <div className='DashboardContainer'>
+                <div className='DashboardContent'>
+                    <h1 className='Heading'>5000+</h1>
+                    <p className='Para'>Problems</p>
+                </div>
+                <div className='DashboardContent'>
+                    <h1 className='Heading'>2500+</h1>
+                    <p className='Para'>Sloved</p>
+                </div>
+                <div className='DashboardContent'>
+                    <h1 className='Heading'>600+</h1>
+                    <p className='Para'>In-Progess</p>
+                </div>
+                <div className='DashboardContent'>
+                    <h1 className='Heading'>1500+</h1>
+                    <p className='Para'>Today Raised</p>
+                </div>
             </div>
         </div>
     )
