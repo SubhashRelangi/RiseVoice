20f6eb4b2ce8e4593b7539acfd8069c17b5abc11
SubhashRelangi <subhashrelangi@gmail.com>
2025-08-28 22:33:09 +0530

    feat: Implement basic routing and global layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import TrackPage from './pages/TrackPage';
+import RaiseComplaintPage from './pages/RaiseComplaintPage';
+import Navigation from './Components/Navigation';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div>
+      <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/track" element={<TrackPage />} />
+        <Route path="/raise-complaint" element={<RaiseComplaintPage />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import Navigation from '../Components/Navigation'
 import HeroSection from '../Components/HomePageComponents/HeroSection'
 import Dashboard from '../Components/HomePageComponents/Dashboard'
 import Departments from '../Components/HomePageComponents/Departments'
 import Services from '../Components/HomePageComponents/Services'
-import Footer from '../Components/Footer'
 
 const HomePage = () => {
   return (
     <div>
-      <Navigation />
       <HeroSection />
       <Dashboard />
       <Departments />
       <Services />
-      <Footer />
     </div>
   )
 }
