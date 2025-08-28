f118b5204c4fb443a5623dac402fab43667d78b0
SubhashRelangi <subhashrelangi@gmail.com>
2025-08-28 17:14:02 +0530

    feat: Add react-router-dom and basic routing
    Installs react-router-dom and sets up a basic routing structure with a single route for the home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
   )
 }
 
