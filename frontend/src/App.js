f83ecf3547b96ab13556ff0e094d62dd9d6393e6
NIKHILKILARAPU <nikhilkilarapu79.com>
2025-08-31 20:03:32 +0530

    login page is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import TrackPage from './pages/TrackPage';
+import RaiseComplaintPage from './pages/RaiseComplaintPage';
+import Navigation from './Components/Header';
+import Footer from './Components/Footer';
+import FloatingButton from './Components/FloatingButton';
 
 const App = () => {
   return (
     <div>
-       <LoginPage/>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/track" element={<TrackPage />} />
+        <Route path="/raise-complaint" element={<RaiseComplaintPage />} />
+      </Routes>
+      <FloatingButton />
+      <Footer />
+
+
     </div>
   )
 }
