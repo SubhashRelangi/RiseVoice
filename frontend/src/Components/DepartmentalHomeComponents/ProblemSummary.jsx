2acf20dd269f26f439fe7ce67d4358b19334ed8c
SubhashRelangi <subhashrelangi@gmail.com>
2025-09-09 07:48:19 +0530

    feat: Simplify ProblemSummary display

--- frontend/src/Components/DepartmentalHomeComponents/ProblemSummary.jsx
+++ frontend/src/Components/DepartmentalHomeComponents/ProblemSummary.jsx
@@ -3,37 +3,22 @@ import styles from './ProblemSummary.module.css';
 
 const ProblemSummary = ({ problems }) => {
   const resolvedProblems = problems.filter(problem => problem.status === 'Resolved' || problem.status === 'Resloved');
-  const inProgressProblems = problems.filter(problem => problem.status === 'In Progress' || problem.status === 'In Progress');
-  const pendingProblems = problems.filter(problem => problem.status === 'Pending' || problem.status === 'Pending');
+  const inProgressProblems = problems.filter(problem => problem.status === 'In Progress');
+  const pendingProblems = problems.filter(problem => problem.status === 'Pending');
 
   return (
     <div className={styles.summaryContainer}>
       <div className={styles.summaryItem}>
         <span className={`${styles.colorBox} ${styles.resolved}`}></span>
         Resolved ({resolvedProblems.length})
-        <ul className={styles.problemList}>
-          {resolvedProblems.map(problem => (
-            <li key={problem._id}>{problem.title}</li>
-          ))}
-        </ul>
       </div>
       <div className={styles.summaryItem}>
         <span className={`${styles.colorBox} ${styles.inProgress}`}></span>
         In Progress ({inProgressProblems.length})
-        <ul className={styles.problemList}>
-          {inProgressProblems.map(problem => (
-            <li key={problem._id}>{problem.title}</li>
-          ))}
-        </ul>
       </div>
       <div className={styles.summaryItem}>
         <span className={`${styles.colorBox} ${styles.Pending}`}></span>
         Pending ({pendingProblems.length})
-        <ul className={styles.problemList}>
-          {pendingProblems.map(problem => (
-            <li key={problem._id}>{problem.title}</li>
-          ))}
-        </ul>
       </div>
     </div>
   );
