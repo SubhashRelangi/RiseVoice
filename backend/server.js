cb8d86510fc8c88e1ad712be2cdf822048662803
SubhashRelangi <subhashrelangi@gmail.com>
2025-09-13 18:18:44 +0530

    feat: Update CORS origin from VoiceUp to RiseVoice in server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.set('trust proxy', true);
 // Middleware
 app.use(cors({
   origin: [
-    "https://voiceupindia.vercel.app",
+    "https://risevoice.vercel.app/",
     "http://localhost:3000"
   ],
   credentials: true,
